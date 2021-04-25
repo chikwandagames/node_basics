39c46ee7507f8b8083e93c0e63a2fa50ec0b9d40
chikwandagames <chikwandagames@gmail.com>
2021-04-25 23:02:42 +0100

    npm packages, colors, .gitignore

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const http = require('http')
 const displayContent = require('./routes')
+const colors = require('colors')
 
 const server = http.createServer(displayContent)
 
 server.listen(3001)
+
+console.log('Hello'.green)
+
+// To run
+// npm run wisdom
